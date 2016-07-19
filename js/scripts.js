ee4bd66d75786975eca4bf99d8e38ac80fd2ca95
Nate Butler <iamnbutler@gmail.com>
2016-07-19 18:53:22 -0400

    Strip out and combine twitch into scripts

--- js/scripts.js
+++ js/scripts.js
@@ -81,3 +81,17 @@ var userFeed = new Instafeed({
   template: '<div class="insta-item"><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></div>'
 });
 userFeed.run();
+
+// Twitch Functions
+
+// Is twitch channel live?
+$.getJSON('https://api.twitch.tv/kraken/streams/then8show', function(channel) {
+    if (channel["stream"] == null) {
+      // Channel is not live
+      console.log("Nate currently isn't live on Twitch, be sure to check back later!");
+    } else {
+      // Channel is live
+      // $('#twitch-live').html('<p><i class="fa fa-fw fa-video-camera fa-live"></i> Nate is currently live on Twitch! Come join the stream <i class="fa fa-fw fa-angle-double-right"</i><p>');
+      $('#nav-stream').html('<i class="fa fa-fw fa-bolt"></i> Stream');
+    }
+});
