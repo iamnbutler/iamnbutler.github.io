92a6f94662fd43bce6f3f855589f8a666b3302c9
Nate Butler <iamnbutler@gmail.com>
2021-04-27 02:19:48 -0700

    add new nav

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,13 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+// // Redirect search to site-scoped google search for now
+// const form = document.querySelector("form");
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const search = form.querySelector("input[type=search]");
+//   search.value = "site:iamnbutler.com " + search.value;
+//   form.submit();
+// });
