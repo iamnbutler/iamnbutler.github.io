99c4d325b1f0c99368c8986be856754598090f4d
Nate Butler <iamnbutler@gmail.com>
2021-11-08 00:31:06 -0500

    Add project page

--- components/Nav/Nav.vue
+++ components/Nav/Nav.vue
@@ -2,11 +2,12 @@
   <div class="font-mono p-8">
     <nav role="navigation" class="flex flex-col p-4">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/post">Post</NavLink>
+      <NavLink to="/post">Posts</NavLink>
+      <NavLink to="/project">Projects</NavLink>
     </nav>
     <div class="sandbox p-4">
-      <p>Color mode: {{ $colorMode.value }}</p>
-      <select v-model="$colorMode.preference">
+      <p>{{ $colorMode.value }}</p>
+      <select v-model="$colorMode.preference" class="border border-black">
         <option value="system">System</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
---
+++ pages/project.vue
@@ -0,0 +1,6 @@
+<template>
+  <Base>
+    <Nav />
+    <div class="prose"><h1>Project</h1></div>
+  </Base>
+</template>
