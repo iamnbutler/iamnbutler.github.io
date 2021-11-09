a818c87bf4b884b21e332e4b6ba78d0af3d7f0e0
Nate Butler <iamnbutler@gmail.com>
2021-11-09 10:50:51 -0500

    Update home

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,10 @@
       <ArticleProse>
         <h1>Hey 👋</h1>
         <p>I'm Nate!</p>
-        <h2>Let's build a website</h2>
-        <p>
-          I've been dreaming of a simple md-driven site that pulls from a data
-          repository for a long time. Oddly enough, my biggest personal
-          inspiration for a website has been the Mac OS notes app for a few
-          years now. So I'm giving it a shot to see where I get.
-        </p>
-        <p>
-          So far I'm *part* of the way there, I've got the `.md` part going...
-          Once the basic site is done I'll pivot to figure out how to pull
-          things directly from my personal
-          <a href="https://github.com/iamnbutler/data">data</a> repo.
-        </p>
         <h2>Why is the site kinda jank?</h2>
         <p>
-          Welcome to my very 'build-in-the-open' personal site. Expect
-          everything not to work!
+          Welcome to my very 'build-in-the-open' personal site. **Expect
+          everything not to work!**
         </p>
         <p>
           I'm a bit addicted to things being perfect before sharing. I've
@@ -30,6 +17,36 @@
           This is my attempt to just throw everything out there, don't worry
           about things being unfinished, and just build everything a bit at a
           time.
+        </p>
+        <h2>Let's build a website</h2>
+        <p>
+          I've been dreaming of a simple md-driven site that pulls from a data
+          repository for a long time. I've become a bit "files-on-disk" believer
+          over the past little while, in a world that is increasingly storing
+          everything in the cloud.
+        </p>
+        <p>
+          Structurally, my biggest personal inspiration for a website has been
+          the Mac OS notes app for a few years now. I love the simple mental model of `folder &rarr; note &rarr; content`,So I'm giving it a shot to
+          see where I get.
+        </p>
+        <p>
+          So far I'm *part* of the way there, I've got the `.md` part going...
+          Once the basic site is done I'll pivot to figure out how to pull
+          things directly from my personal
+          <a href="https://github.com/iamnbutler/data">data</a> repo.
+        </p>
+        <p>
+          Here is a rough
+          <a
+            href="https://www.figma.com/file/VNpgDenh0jr5gin1E5YoJt/v-14-moodboard?node-id=0%3A1"
+            class="underline"
+            >~mood board~</a
+          >
+          of some themes I have in my head. I don't know how much will influence
+          the site in the end... I'm starting from structure and content first
+          before style, as I tend to get lost in the visuals and never return to
+          finish the rest 😅
         </p>
         <h2>Anyways...</h2>
         <p>Hope you can find something fun in your time here!</p>
