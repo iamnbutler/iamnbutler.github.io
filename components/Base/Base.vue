db0c280e1f89365b77afa8277658c4eec6653218
Nate Butler <iamnbutler@gmail.com>
2021-11-17 01:18:31 -0500

    unbreak mobile layout

--- components/Base/Base.vue
+++ components/Base/Base.vue
@@ -1,17 +1,9 @@
 <template>
-  <main>
+  <main class="min-h-screen bg-white dark:bg-black">
     <div
-      class="flex flex-col font-mono text-black bg-gray-200 app-container dark:text-white dark:bg-gray-900 md:grid"
+      class="flex flex-col font-mono text-black bg-gray-200 app-container dark:text-white dark:bg-gray-900 md:h-screen md:grid"
       :class="[{ two: subnav === false, three: subnav === true }]"
     >
-      <div
-        v-if="header"
-        class="flex items-center pl-4 pr-4 tracking-wider text-gray-500 bg-white app-header dark:text-gray-400 dark:bg-black"
-      >
-        <slot name="header">
-          <p>natebutler</p>
-        </slot>
-      </div>
       <div v-if="nav" class="p-4 leading-tight bg-white app-nav dark:bg-black">
         <slot name="nav">
           <Nav />
@@ -24,6 +16,14 @@
         <slot name="subnav"></slot>
       </div>
       <slot></slot>
+      <div
+        v-if="header"
+        class="flex items-center pl-4 pr-4 tracking-wider text-gray-500 bg-white app-header dark:text-gray-400 dark:bg-black"
+      >
+        <slot name="header">
+          <p>natebutler</p>
+        </slot>
+      </div>
     </div>
   </main>
 </template>
@@ -48,12 +48,7 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  overflow: hidden;
-}
-
 .app-container {
-  height: 100vh;
   width: 100vw;
   grid-template-columns: 1.5fr 2.5fr 8fr;
   grid-template-rows: 1fr 2em;
