41edf5742061067164d4cb02b95520a1fd125eb7
Nate Butler <iamnbutler@gmail.com>
2022-01-02 00:39:02 -0500

    Add list of posts to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,38 @@
-import { allDocs } from '.contentlayer/data'
+import { allDocs } from ".contentlayer/data";
 
 type StaticProps = {
-  navInfo: { title: string; path: string }[]
-}
+  navInfo: { title: string; path: string; category: string; date: string }[];
+};
 
 const DocPage: React.FC<StaticProps> = ({ navInfo }) => {
-  
-
   return (
     <div>
-      <div style={{ display: 'flex', paddingBottom: 10, borderBottom: '1px solid #eee' }}>
-        {navInfo.map(({ path, title }) => (
-          <a key={path} href={path} style={{ paddingRight: 6 }}>
-            {title}
-          </a>
-        ))}
-      </div>
       <h1>hello</h1>
+      <p>welcome to my corner of the internet</p>
+      <ul>
+        {navInfo.map(({ path, title, category, date }) => (
+          <li className="flex">
+            <a key={path} href={path}>
+              <h3>{title}</h3>
+            </a>
+            <p>{category}</p>
+            <p>{date}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default DocPage
+export default DocPage;
 
 export const getStaticProps = (): { props: StaticProps } => {
+  const navInfo = allDocs.map((_) => ({
+    title: _.title,
+    path: `/f/${_._raw.flattenedPath}`,
+    category: _.title,
+    date: _.title,
+  }));
 
-  const navInfo = allDocs.map((_) => ({ title: _.title, path: `/f/${_._raw.flattenedPath}` }))
-
-  return { props: { navInfo } }
-}
+  return { props: { navInfo } };
+};
