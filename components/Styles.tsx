679d38b04b0032be71e6d400a6fcc56add5f19df
Nate Butler <iamnbutler@gmail.com>
2022-06-28 01:52:21 +0100

    Update theme to rose pine

--- components/Styles.tsx
+++ components/Styles.tsx
@@ -1,8 +1,8 @@
 export const navLinkStyle = {
   active:
-    "text-base0B bg-base0B/10 hover:bg-base0B/20 border-base0B/40 shadow-inner hover:shadow-inner",
+    "text-base0D bg-base0D/10 hover:bg-base0D/20 border-base0D/40 shadow-inner hover:shadow-inner",
   inactive:
-    "hover:bg-base0B/10 hover:border-base0B/20 hover:text-base0B text-base0B/70",
+    "hover:bg-base0D/10 hover:border-base0D/20 hover:text-base0D text-base0D/70",
   common:
-    "text-base flex font-bold px-4 py-1.5 rounded-lg border border-transparent shadow-base0B/10 hover:shadow-base0B/10 hover:shadow-xl transition-all",
+    "text-base flex font-bold px-4 py-1.5 rounded-lg border border-transparent shadow-base0D/10 hover:shadow-base0D/10 hover:shadow-xl transition-all",
 }
