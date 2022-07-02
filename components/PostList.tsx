3f0d3f9513c359a12dc1bd8f0f16df61dc6ccc55
Nate Butler <iamnbutler@gmail.com>
2022-07-03 00:23:51 +0100

    Simplify mvp post list

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -3,60 +3,26 @@ import Link from "next/link"
 import { format, parseISO } from "date-fns"
 import { useRouter } from "next/router"
 import { navLinkStyle } from "./Styles"
-import { CodeIcon, PenNibIcon, RulerIcon, MusicIcon } from "assets/Icons"
-import { ButtonItem } from "./Button"
 import Button from "./Button"
 
 let posts = allPosts.reverse()
 
-let filters: ButtonItem[] = [
-  {
-    name: "Music",
-    icon: <MusicIcon />,
-    tooltip: "Show all music related posts",
-  },
-  {
-    name: "Writing",
-    icon: <PenNibIcon />,
-    tooltip: "Show all writing",
-  },
-  {
-    name: "Design",
-    icon: <RulerIcon />,
-    tooltip: "Show all design related posts",
-  },
-  {
-    name: "Code",
-    icon: <CodeIcon />,
-    tooltip: "Show all code related posts",
-  },
-]
+interface PostListProps { }
 
-interface PostListProps {}
-
-export default function PostList({}: PostListProps) {
+export default function PostList({ }: PostListProps) {
   let currentPath = useRouter().asPath
 
   return (
-    <div className="flex flex-col md:w-72 lg:w-96 flex-shrink-0 relative h-screen overflow-y-scroll styled-scrollbars">
-      <header className="sticky top-0 bg-base00/70 backdrop-blur-md h-12 flex justify-between items-center py-8 z-20 px-4">
-        <h2 className="font-bold ml-4">Posts</h2>
-        <menu className="flex space-x-1 h-8">
-          {filters.map((filter) => (
-            <Button button={filter} key={filter.name} />
-          ))}
-        </menu>
-      </header>
-      <div className="px-4">
+    <div className="flex flex-col w-full flex-shrink-0 relative h-screen overflow-y-scroll styled-scrollbars">
+      <div className="p-4">
         {posts.map((post) => (
           <Link href={`/post/${post.slug}`} key={post.uuid}>
             <a
               className={`
-            ${
-              currentPath === `/post/${post.slug}`
-                ? navLinkStyle.active
-                : navLinkStyle.inactive
-            }
+            ${currentPath === `/post/${post.slug}`
+                  ? navLinkStyle.active
+                  : navLinkStyle.inactive
+                }
             ${navLinkStyle.common}
             flex flex-col space-y-1 mb-2
             `}
