4a9267fe5f7fd39445846aa57c890d26f6407454
Nate Butler <iamnbutler@gmail.com>
2022-08-03 03:18:43 -0400

    Fix unescaped `'`s

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <header className="flex flex-col max-w-xl mx-auto my-16 px-4 py-2 prose">
           <h1 className="text-3xl">Still WIP, but getting something out there.</h1>
           <p>Welcome to my very build-in-the-open personal site. <i>Expect everything not to work!</i></p>
-          <p>I'm a bit addicted to things being perfect before sharing. I've actually never shipped a personal site because of this.</p>
-          <p>This is my attempt to just throw everything out there, don't worry about things being unfinished, and just build everything a bit at a time.</p>
+          <p>I&apos;m a bit addicted to things being perfect before sharing. I&apos;ve actually never shipped a personal site because of this.</p>
+          <p>This is my attempt to just throw everything out there, don&apos;t worry about things being unfinished, and just build everything a bit at a time.</p>
 
           <h2>Some more normal bio info:</h2>
           <p>
