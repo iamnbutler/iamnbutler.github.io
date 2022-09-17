7e9cef471af50cd399134646a5f6080e2387fb60
Nate Butler <iamnbutler@gmail.com>
2022-09-16 23:46:39 -0400

    Update index

--- pages/index.vue
+++ pages/index.vue
@@ -2,69 +2,11 @@
   <Base>
     <BaseContent>
       <ArticleProse>
-        <h1>Hey 👋</h1>
-        <p>I'm Nate!</p>
-        <h2>Why is the site kinda jank?</h2>
+        <h1>I'm Nate 👋</h1>
+        <p>Thanks for stopping by. This site is a work in progress, but feel free to explore to your heart's content 🙂</p>
         <p>
-          Welcome to my very 'build-in-the-open' personal site. **Expect
-          everything not to work!**
-        </p>
-        <p>
-          I'm a bit addicted to things being perfect before sharing. I've
-          actually never shipped a personal site because of this.
-        </p>
-        <p>
-          This is my attempt to just throw everything out there, don't worry
-          about things being unfinished, and just build everything a bit at a
-          time.
-        </p>
-        <h2>Let's build a website</h2>
-        <p>
-          I've been dreaming of a simple md-driven site that pulls from a data
-          repository for a long time. I've become a bit "files-on-disk" believer
-          over the past little while, in a world that is increasingly storing
-          everything in the cloud.
-        </p>
-        <p>
-          Structurally, my biggest personal inspiration for a website has been
-          the Mac OS notes app for a few years now. I love the simple mental model of `folder &rarr; note &rarr; content`,So I'm giving it a shot to
-          see where I get.
-        </p>
-        <p>
-          So far I'm *part* of the way there, I've got the `.md` part going...
-          Once the basic site is done I'll pivot to figure out how to pull
-          things directly from my personal
-          <a href="https://github.com/iamnbutler/data">data</a> repo.
-        </p>
-        <p>
-          Here is a rough
-          <a
-            href="https://www.figma.com/file/VNpgDenh0jr5gin1E5YoJt/v-14-moodboard?node-id=0%3A1"
-            class="underline"
-            >~mood board~</a
-          >
-          of some themes I have in my head. I don't know how much will influence
-          the site in the end... I'm starting from structure and content first
-          before style, as I tend to get lost in the visuals and never return to
-          finish the rest 😅
-        </p>
-        <h2>Anyways...</h2>
-        <p>Hope you can find something fun in your time here!</p>
-        <p>
-          You can check out my
-          <a
-            href="https://github.com/iamnbutler/iamnbutler.github.io/blob/main/.meta/tech.md"
-            class="underline"
-            >tech.md</a
-          >
-          to see all the stuff I'll be exploring on this site.
-        </p>
-        <p>
-          For a less broken experience for now, you can follow me on
-          <a href="https://twitter.com/iamnbutler" class="underline">twitter</a>
-          here.
-        </p>
-        <p>See you around!</p>
+          Say Hi on 
+          <a href="https://twitter.com/iamnbutler" class="underline">twitter</a>, and see you around!</p>
       </ArticleProse>
     </BaseContent>
   </Base>
