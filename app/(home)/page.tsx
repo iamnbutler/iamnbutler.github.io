29b5ac148172b6fe228ef3472e1bb49d827d163d
Nate Butler <iamnbutler@gmail.com>
2023-03-18 14:19:46 -0400

    Add initial cards

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -55,7 +55,7 @@ const NavItem = ({ icon, href, label, description }: INavItem) => (
 );
 
 const GridItem = ({ children }: PropsWithChildren) => (
-    <section className="p-5 bg-white min-h-[760px]">{children}</section>
+    <section className="p-5 bg-white min-h-[760px] flex flex-col justify-between">{children}</section>
 );
 
 export default function Home() {
@@ -81,8 +81,45 @@ export default function Home() {
                     </nav>
                 </header>
             </GridItem>
-            <GridItem>Column 2</GridItem>
-            <GridItem>Column 3</GridItem>
+            <GridItem>
+                <div className="prose shrink-1">
+                    <h2>hey ✌️</h2>
+                    <p className="text-lg font-semibold italic">I’m nate butler, a designer & maker enabling people’s creativity and ability share knowledge.</p>
+                    <p>I want to help people create the things important to them—To empower them to create something themselves and feel the euphoria it brings. My goal is always to help the people around me level up, in their careers & lives.</p>
+                    <p>I post about all types of things here. You will find a mix of work, top of mind, reflections, & process. Enjoy!</p>
+                </div>
+                <footer className="shrink-0 flex justify-between">
+                    <a href="https://twitter.com/iamnbutler" className="underline">more about me?</a>
+                    <p>avatar here</p>
+                </footer>
+            </GridItem>
+            <GridItem>
+                <div className="prose shrink-1">
+                    <h2>work 🎒✌️</h2>
+                    <ul>
+                        <li>
+                            <h3 className="my-0">Design at Zed</h3>
+                            <p>Building the world's best code editor - 2021++</p>
+                        </li>
+                        <li>
+                            <h3 className="my-0">Internal Tools at Facebook</h3>
+                            <p>Helping product teams build more efficiently – 2020</p>
+                        </li>
+                        <li>
+                            <h3 className="my-0">Avatars at Facebook</h3>
+                            <p>The groundwork for an identity system for billions – 2018</p>
+                        </li>
+                        <li>
+                            <h3 className="my-0">News Feed at Facebook</h3>
+                            <p>In an increasingly visual feed, how can we help people level up their posts? – 2016</p>
+                        </li>
+                    </ul>
+                </div>
+                <footer className="shrink-0 flex justify-between">
+                    <p>more work &rarr;</p>
+                    <a href="https://read.cv/natebutler" className="underline">read.cv</a>
+                </footer>
+            </GridItem>
             <GridItem>Column 4</GridItem>
             <GridItem>Column 4</GridItem>
             <GridItem>Column 4</GridItem>
