1ace63b3eb667dd7bdffe8267116d476d1975b14
Nate Butler <iamnbutler@gmail.com>
2023-03-19 16:23:45 -0400

    Add zed.dev

--- content/index.ts
+++ content/index.ts
@@ -4,11 +4,13 @@ import { figGen } from "./fig-gen";
 import { IProject } from "@/types/project";
 import { zedPublicBeta } from "./zed-public-beta";
 import { dalleWorlds } from "./dalleWorlds";
+import { zedDotDev } from "./zed-dot-dev";
 
 export const projects: IProject[] = [
     zedPublicBeta,
     dalleWorlds,
     designDocsV2,
+    zedDotDev,
     figGen,
     mixblocks,
 ];
