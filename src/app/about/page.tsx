59f2ee6f3d99b9ab282e16adf33293cd0c85d584
Nate Butler <iamnbutler@gmail.com>
2023-08-19 11:16:51 -0400

    Finish content once-over

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -103,7 +103,7 @@ export default function About() {
                             icon={MailIcon}
                             className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
                         >
-                            spencer@planetaria.tech
+                            iamnbutler(at)gmail.com
                         </SocialLink>
                     </ul>
                 </div>
