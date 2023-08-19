b36f2dfebdf41322592f9898aa58bb396bd33376
Nate Butler <iamnbutler@gmail.com>
2023-08-19 12:45:56 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,8 +29,7 @@ export function Footer() {
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                {/* <NavLink href="/uses">Uses</NavLink> */}
               </div>
               <p className="text-sm text-zinc-200 dark:text-zinc-700">
                 &copy; {new Date().getFullYear()} Nate Butler. All rights
