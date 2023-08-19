7fddf4b7f8e5f532ea34e2a23a0c56c8b361d3a2
Nate Butler <iamnbutler@gmail.com>
2023-08-19 11:17:12 -0400

    Format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,39 +6,38 @@ import { Layout } from '@/components/Layout'
 import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
-    title: {
-        template: '%s - Nate Butler',
-        default:
-            'Nate Butler - Vision Designer & Design Engineer',
+  title: {
+    template: '%s - Nate Butler',
+    default: 'Nate Butler - Vision Designer & Design Engineer',
+  },
+  description:
+    'I’m a Nate, a Vision Designer & Design Engineer living in Toronto, Canada 🇨🇦 I love creating things to help people express themselves.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
     },
-    description:
-        'I’m a Nate, a Vision Designer & Design Engineer living in Toronto, Canada 🇨🇦 I love creating things to help people express themselves.',
-    alternates: {
-        types: {
-            'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-        },
-    },
+  },
 }
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-            <link
-                rel="icon"
-                href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚰️</text></svg>"
-            />
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚰️</text></svg>"
+      />
 
-            <body className="flex h-full bg-zinc-50 dark:bg-black">
-                <Providers>
-                    <div className="flex w-full">
-                        <Layout>{children}</Layout>
-                    </div>
-                </Providers>
-            </body>
-        </html>
-    )
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
 }
