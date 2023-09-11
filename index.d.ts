7a994f0bc9410aa480311d54793c311f37d4f42c
Nate Butler <iamnbutler@gmail.com>
2023-09-10 20:36:45 -0400

    Clean out unrelated files

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-// import { IProject } from "./types/project";
-
-// declare module '*.mdx' {
-//     // Export project meta from all mdx files
-//     export const meta: IProject
-
-//     let MDXComponent: (props) => JSX.Element;
-//     export default MDXComponent;
-// }
