30e505de38d82d74e5175137b8a6649ff6c09f15
Nate Butler <iamnbutler@gmail.com>
2024-06-17 19:12:22 -0400

    wip

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,17 +5,40 @@ const fragmentKind = {
   IMAGES: "images",
 } as const;
 
+export type FragmentKind = keyof typeof fragmentKind;
+
+const tags = {
+  WEB_DEV: "web-dev",
+  PYTHON: "python",
+  JAVASCRIPT: "js",
+  DESIGN: "design",
+};
+
+export type Tag = keyof typeof tags;
+
 const fragmentCollection = defineCollection({
   type: "content",
   schema: z.object({
     date_published: z.date(),
     title: z.string(),
     preview: z.string(),
-    tags: z.array(z.string()),
+    tags: z.array(z.nativeEnum(tags)),
     kind: z.nativeEnum(fragmentKind),
+    set: z.string().optional(),
+  }),
+});
+
+const fragmentSetCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    fragments: z.array(z.string()),
+    tags: z.array(z.nativeEnum(tags)),
   }),
 });
 
 export const collections = {
   fragment: fragmentCollection,
+  fragmentSet: fragmentSetCollection,
 };
---
+++ src/content/fragmentSet/astro_site.md
@@ -0,0 +1,8 @@
+---
+id: "astro_site"
+title: "Astro Site 2024"
+fragments: ["00", "01", "02"]
+tags: ["web-dev", "js", "python"]
+---
+
+Let's go an adventure to build this site with Astro!
