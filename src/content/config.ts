e067d39aadbaaf8d265a724993fe92a668b71fbe
Nate Butler <iamnbutler@gmail.com>
2024-06-17 15:51:13 -0400

    Add fragment kind

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,10 @@
 import { z, defineCollection } from "astro:content";
 
+const fragmentKind = {
+  POST: "post",
+  IMAGES: "images",
+} as const;
+
 const fragmentCollection = defineCollection({
   type: "content",
   schema: z.object({
@@ -7,6 +12,7 @@ const fragmentCollection = defineCollection({
     title: z.string(),
     preview: z.string(),
     tags: z.array(z.string()),
+    kind: z.nativeEnum(fragmentKind),
   }),
 });
 
