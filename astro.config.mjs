74b61c7c8d91abbf0112d488bb2cbb4e5d1b0f48
Nate Butler <iamnbutler@gmail.com>
2024-06-18 12:11:29 -0400

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import gruvboxDark from "./shiki_gruvbox_dark.json";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   markdown: {
     shikiConfig: {
       defaultColor: false,
