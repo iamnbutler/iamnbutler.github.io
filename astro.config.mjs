7006a9661468f5cf7612935c5f64ca152d09f27a
Nate Butler <iamnbutler@gmail.com>
2025-04-02 20:29:50 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://iamnbutler.github.io",
+  site: "https://nate.rip",
   integrations: [mdx(), sitemap(), tailwind()],
 });
